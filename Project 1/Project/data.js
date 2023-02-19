fe3c0a32e8dc5fd3be08b6b70df98968c73e8abd
CharissaYeong <122376282+CharissaYeong@users.noreply.github.com>
2023-02-20 06:52:34 +0800

    Add files via upload

---
+++ Project 1/Project/data.js
@@ -0,0 +1,35 @@
+// Data paths
+const weeklyPath = "https://data.gov.sg/api/action/datastore_search?resource_id=ef7e44f1-9b14-4680-a60a-37d2c9dda390&limit=20070" // Weekly infectious disease records
+
+// Weekly data
+async function loadData() {
+    const response = await axios.get(weeklyPath);
+    return response.data.result.records
+}
+
+console.log(loadData())
+
+async function transformData() {
+
+    // Store promise results in variable
+    let data = await loadData();
+    console.log(Array.isArray(data)); //check if it is array
+    console.log(data)
+
+    // Filter only Dengue cases
+    let filteredData = data.filter(function (dataPoint) {
+        return dataPoint.disease == "Dengue Fever";
+
+    }); console.log(filteredData);
+
+    // Map the data
+    let mappedData = filteredData.map(function(dataPoint){
+        return {
+            "E-Week": dataPoint.epi_week,
+            "Number_of_Cases": dataPoint
+        }
+    }); console.log(mappedData);
+
+}
+
+transformData()
