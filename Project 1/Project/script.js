c29766520283e055a8115526fb8fa7ced6551e09
Charissa Yeong <donutdandy@gmail.com>
2023-02-24 21:22:00 +0800

    sync

--- Project 1/Project/script.js
+++ Project 1/Project/script.js
@@ -2,7 +2,7 @@ let range = document.getElementById('customRange3').value
 
 console.log(range)
 
-let btn = document.getElementByName('form-check-input')
+let btn = document.getElementByName('')
 console.log(btn)
 
 let btn2 = document.getElementByName('weekView')
