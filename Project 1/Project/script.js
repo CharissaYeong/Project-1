439a4d62f817d7163d4d8bc6302fa09b4e9b5ec1
Charissa Yeong <donutdandy@gmail.com>
2023-03-07 09:05:50 +0800

    Deleted extra code

--- Project 1/Project/script.js
+++
@@ -1,9 +0,0 @@
-
-
-// let full_radio = document.getElementById('full_view_q')
-// let yr_radio = document.getElementById('year_view_q')
-// let year_range = document.getElementById('year_range_q')
-// let clear = document.getElementById('clear_q')
-// let all_years = document.getElementById('select_all')
-
-// console.log(full_radio.checked, yr_radio.checked, year_range.value)
